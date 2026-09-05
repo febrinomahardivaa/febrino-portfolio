81db5d053e3a45a6d34fd11d16d524dca122cf56
Febrino Mahardiva Akbar <febrinomahardivaa@Febrinos-MBP.local>
2026-09-06 00:28:47 +0700

    add vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/febrino-portfolio',
+  base: process.env.VITE_BASE_PATH || '/febrino-portfolio',
 })
