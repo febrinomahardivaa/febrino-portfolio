13478123d0fffdccdb818e429dab76743d1ac6a2
Febrino Mahardiva Akbar <febrinomahardivaa@Febrinos-MBP.local>
2026-09-06 02:08:20 +0700

    fixing navbar title

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div class="navbar bg-base-300 shadow-sm">
-        <a class="btn btn-ghost text-xl">daisyUI</a>
+        <a class="btn btn-ghost text-xl">Febrino</a>
       </div>
     </>
   )
